b237198f47fdb5dccc6c40f8d0dbd597ccca636e
coberlin96 <oberlin.connor@gmail.com>
2022-07-11 17:29:15 -0400

    First Commit

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Dashboard =() =>{
+    return (
+        <div>
+            <h1>Dashboard Goes Here</h1>
+        </div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home, Dashboard, Signin } from './components'
+import './styles.css'
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={< Home title = {'car.inventory'}/>}/>
+        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path='/signin' element={<Signin/>}/>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
